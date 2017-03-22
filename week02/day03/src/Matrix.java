a71c598d45ab3c0310eea8f6811b2a31e0079958
balintvecsey <balint.vecsey@gmail.com>
2017-03-22 15:34:38 +0100

    add three exercises

---
+++ week02/day03/src/Matrix.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Bálint on 2017. 03. 22..
+ */
+public class Matrix {
+  // - Create (dynamically) a two dimensional array
+  //   with the following matrix. Use a loop!
+  //
+  //   1 0 0 0
+  //   0 1 0 0
+  //   0 0 1 0
+  //   0 0 0 1
+  //
+  // - Print this two dimensional array to the output
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ week02/day03/src/printAll.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Bálint on 2017. 03. 22..
+ */
+public class printAll {
+  // - Create an array variable named `af`
+  //   with the following content: `[4, 5, 6, 7]`
+  // - Print all the elements of `af`
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+    System.out.println(Arrays.toString(af));
+  }
+}
