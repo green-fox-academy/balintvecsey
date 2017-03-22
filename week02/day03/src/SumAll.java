23a2d34e134796bdc06a752e531b0e96117cc8e2
balintvecsey <balint.vecsey@gmail.com>
2017-03-22 17:18:38 +0100

    add last exercises

---
+++ week02/day03/src/SumAll.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Bálint on 2017. 03. 22..
+ */
+public class SumAll {
+
+  public static void main(String[] args) {
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+    int[] ai = {3, 4, 5, 6, 7};
+    int sum = 0;
+
+    for (int i = 0; i < sum; i++) {
+      sum += ai[i];
+    }
+  }
+}
