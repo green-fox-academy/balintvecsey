d02dc6edbb149ce2b97ba0cf35bdc3a0d3c79128
balintvecsey <balint.vecsey@gmail.com>
2017-03-23 17:20:29 +0100

    add some exercises

---
+++ week02/day02/src/FiizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by Bálint on 2017. 03. 23..
+ */
+public class FiizzBuzz {
+  // Write a program that prints the numbers from 1 to 100.
+  // But for multiples of three print “Fizz” instead of the number
+  // and for the multiples of five print “Buzz”.
+  // For numbers which are multiples of both three and five print “FizzBuzz”.
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i = 3) {
+
+      }
+      System.out.println(i);
+    }
+  }
+}
--- week02/day02/src/Pyramid.java
+++ week02/day02/src/Pyramid.java
@@ -3,28 +3,31 @@ import java.util.Scanner;
 /**
  * Created by Bálint on 2017. 03. 23..
  */
-public class DrawTriangle {
+public class Pyramid {
   // Write a program that reads a number from the standard input, then draws a
-  // triangle like this:
+  // pyramid like this:
   //
-  // *
-  // **
-  // ***
-  // ****
   //
-  // The triangle should have as many lines as the number was
-
+  //    *
+  //   ***
+  //  *****
+  // *******
+  //
+  // The pyramid should have as many lines as the number was
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Write down a number");
 
     int a = scanner.nextInt();
 
-    for (int i = 0; i <= a; i++) {
-      for (int j = 0; j < i; j++) {
+    for (int i = 0; i < a; i++) {
+      for (int j = 0; j < a - i; j++) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k <= 2*i; k++){
         System.out.print("*");
       }
-      System.out.println();
+      System.out.println("");
     }
   }
 }
