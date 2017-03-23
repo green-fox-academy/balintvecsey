ce4f65bc08ec38f43dd2c540421a1285201c6b82
balintvecsey <balint.vecsey@gmail.com>
2017-03-23 15:39:19 +0100

    add two exercises

--- week02/day04/src/AppendLetter.java
+++ week02/day04/src/AppendLetter.java
@@ -7,7 +7,12 @@ public class AppendLetter{
   public static void main(String... args){
     ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
     // Add "a" to every string in the far list.
-    //app = far.add("a");
+
+    far.set(0, "kutya");
+    far.set(1, "macska");
+    far.set(2, "kacsa");
+    far.set(3, "róka");
+    far.set(4, "halacska");
     System.out.println(far);
   }
 }
