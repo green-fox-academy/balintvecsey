c7132d0b7bbf9fc36b7b1517920411ab1b519124
balintvecsey <balint.vecsey@gmail.com>
2017-03-28 00:27:06 +0200

    some editing

--- week03/day01/src/Lotto.java
+++ week03/day01/src/Lotto.java
@@ -2,10 +2,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -23,7 +20,6 @@ public class Lotto {
 
       ArrayList<String>  lottoNumbers = new ArrayList<>();
 
-      //HashMap commons = new HashMap<String, String>();
       for (String line : lines) {
         String[] splitted = line.split(";");
 
@@ -32,10 +28,6 @@ public class Lotto {
           lottoNumbers.add(splitted[i]);
         }
       }
-
-      /*for (int i = 1; i <= 90; i++) {
-        commons.put(i, Collections.frequency(lottoNumbers, Integer.toString(i)));
-      }*/
 
       int[][] sortedCommons = new int[90][2];
       for (int i = 1; i <= 90; i++) {
@@ -54,7 +46,7 @@ public class Lotto {
       }
 
       for (int i = 0; i < 5; i++) {
-        System.out.println("The " + (i + 1) + ". most common lotto number is: " + sortedCommons[i][0]);
+        System.out.println("The " + (i + 1) + ". most common lotto number is: " + sortedCommons[i][0] + '.');
       }
 
     } catch (Exception e) {
