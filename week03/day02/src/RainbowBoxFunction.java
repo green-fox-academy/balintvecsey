779517fd0ab9b467d1e9460cc8373009e9bb9529
balintvecsey <balint.vecsey@gmail.com>
2017-03-28 14:11:58 +0200

    finished rainbow

--- week03/day02/src/RainbowBoxFunction.java
+++ week03/day02/src/RainbowBoxFunction.java
@@ -14,20 +14,26 @@ public class RainbowBoxFunction {
     // the square size, and the fill color,
     // and draws a square of that size and color to the center of the canvas.
     // create a loop that fills the canvas with rainbow colored squares.
+    int size = 300;
 
-
+    for (int i = 0; i < 30 ; i++) {
+      int color = (int) (Math.random() * 16777215);
+      size -= 30;
+      rainbow(size, color, graphics);
     }
 
-  public static void rainbow(int x, int color, Graphics g) {
+  }
+
+  public static void rainbow(int size, int color, Graphics g) {
     g.setColor(new Color(color));
-    g.fillRect(150 - (x / 2), 150 - (x / 2), x, x);
+    g.fillRect(150 - (size / 2), 150 - (size / 2), size, size);
 
   }
 
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setSize(new Dimension(320, 345));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
