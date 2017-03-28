9e8f775abe88079d5e22507f5e52049d2dd50ef2
balintvecsey <balint.vecsey@gmail.com>
2017-03-28 13:24:47 +0200

    create function

---
+++ week03/day02/src/RainbowBoxFunction.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Bálint on 2017. 03. 28..
+ */
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class RainbowBoxFunction {
+
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // create a loop that fills the canvas with rainbow colored squares.
+
+
+    }
+
+  public static void rainbow(int x, int color, Graphics g) {
+    g.setColor(new Color(color));
+    g.fillRect(150 - (x / 2), 150 - (x / 2), x, x);
+
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
