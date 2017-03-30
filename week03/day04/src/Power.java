f32e6850ae4b46a5ba23c691c4547d1387bb737f
balintvecsey <balint.vecsey@gmail.com>
2017-03-30 22:52:45 +0200

    now it can zero power

--- week03/day04/src/Power.java
+++ week03/day04/src/Power.java
@@ -6,16 +6,15 @@ public class Power {
   // Given base and n that are both 1 or more, compute recursively (no loops)
   // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
   public static void main(String[] args) {
-    System.out.println(nPower(3,3));
+    System.out.println(nPower(3, 3));
   }
 
   public static int nPower(int n, int x) {
     if (n == 1) {
       return 1;
-    } else if (x == 1){
-      return n;
-    }
-    else {
+    } else if (x == 0) {
+      return 1;
+    } else {
       return n * (nPower(n, x - 1));
     }
   }
