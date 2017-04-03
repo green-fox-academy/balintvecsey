cb7dfe5e82d761eae0e0f2fb289f25904175f9d0
balintvecsey <balint.vecsey@gmail.com>
2017-04-03 16:01:44 +0200

    remove comment

--- week04/day01/src/Counter.java
+++ week04/day01/src/Counter.java
@@ -1,14 +1,6 @@
 /**
  * Created by Bálint on 2017. 04. 03..
  */
-    /*Create Counter class
-    which has an integer field value
-    when creating it should have a default value 0 or we can specify it when creating
-    we can add(number) to this counter another whole number
-    or we can add() without parameters just increasing the counter's value by one
-    and we can get() the current value
-    also we can reset() the value to the initial value*/
-
 public class Counter {
   int a;
   int current;
