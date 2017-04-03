c522fe6e8e14651848feec381faf036d126344a7
balintvecsey <balint.vecsey@gmail.com>
2017-04-03 15:55:42 +0200

    add Counter, Teacher and Student

---
+++ week04/day01/src/Student.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Bálint on 2017. 04. 03..
+ */
+public class Student {
+
+  public void learn(){
+    System.out.println("Learning.");
+  }
+
+  public void question(Teacher teacher){
+    teacher.answer();
+  }
+}
---
+++ week04/day01/src/Teacher.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Bálint on 2017. 04. 03..
+ */
+public class Teacher {
+
+  public void teach(Student student){
+    student.learn();
+  }
+
+  public void answer(){
+    System.out.println("It's my answer.");
+  }
+}
