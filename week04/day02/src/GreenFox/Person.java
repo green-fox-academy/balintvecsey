9cc788f8182f414962c4e2c35830fb2a16cbdf82
balintvecsey <balint.vecsey@gmail.com>
2017-04-04 11:47:23 +0200

    add GreenFoxApp

---
+++ week04/day02/src/GreenFox/Person.java
@@ -0,0 +1,30 @@
+package GreenFox;
+
+/**
+ * Created by Bálint on 2017. 04. 03..
+ */
+public class Person {
+  String name;
+  int age;
+  String gender;
+
+  public Person(String name, int age, String gender){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person(){
+    name = "Jane Doe";
+    age = 30;
+    gender = "female";
+  }
+
+  public void introduce(){
+    System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+  }
+
+  public void getGoal(){
+    System.out.println("My goal is: Live for the moment!");
+  }
+}
