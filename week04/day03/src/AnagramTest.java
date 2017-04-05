02c294b1b2921753082929b58661faa593d408f7
balintvecsey <balint.vecsey@gmail.com>
2017-04-05 14:11:11 +0200

    change assertEqual to assertTrue or assertFalse

--- week04/day03/src/AnagramTest.java
+++ week04/day03/src/AnagramTest.java
@@ -10,18 +10,18 @@ public class AnagramTest {
   @Test
   public void TestTrue() {
     Anagram a = new Anagram();
-    assertEquals(true, a.checkAnagram("baba", "abba"));
+    assertTrue(a.checkAnagram("baba", "abba"));
   }
 
   @Test
   public void TestFalse() {
     Anagram a = new Anagram();
-    assertEquals(false, a.checkAnagram("baba", "dada"));
+    assertFalse(a.checkAnagram("baba", "dada"));
   }
 
   @Test
   public void TestUppercase() {
     Anagram a = new Anagram();
-    assertEquals(true, a.checkAnagram("BAba", "abba"));
+    assertTrue(a.checkAnagram("Baba", "aBBa"));
   }
 }
