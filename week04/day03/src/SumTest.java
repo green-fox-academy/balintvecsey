1e3b9b84fcf6efdd2f756cb07c99b3471fe02226
balintvecsey <balint.vecsey@gmail.com>
2017-04-05 12:35:16 +0200

    add new test cases

--- week04/day03/src/SumTest.java
+++ week04/day03/src/SumTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import org.junit.Test;
 
 /**
  * Created by Bálint on 2017. 04. 05..
@@ -8,7 +9,7 @@ import java.util.ArrayList;
 public class SumTest {
 
   @org.junit.Test
-  public void getSum() {
+  public void TestFirst() {
     ArrayList<Integer> numbers = new ArrayList<>();
     numbers.add(1);
     numbers.add(2);
@@ -18,4 +19,39 @@ public class SumTest {
     assertEquals(7, sum.getSum());
   }
 
+  @Test
+  public void TestEmpty() {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    Sum sum = new Sum(numbers);
+
+    assertEquals(0, sum.getSum());
+  }
+
+  @Test
+  public void TestOneE() {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(3);
+    Sum sum = new Sum(numbers);
+
+    assertEquals(3, sum.getSum());
+  }
+
+  @Test
+  public void TestMultipleE() {
+    ArrayList<Integer> numbers = new ArrayList<>();
+    numbers.add(23);
+    numbers.add(7);
+    numbers.add(12);
+    Sum sum = new Sum(numbers);
+
+    assertEquals(42, sum.getSum());
+  }
+
+  @Test
+  public void TestNull() {
+    ArrayList<Integer> numbers = null;
+    Sum sum = new Sum(numbers);
+
+    assertEquals(0, sum.getSum());
+  }
 }
