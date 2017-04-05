caedbced9e8e722b4bd86ed683ce4a882696f1bb
balintvecsey <balint.vecsey@gmail.com>
2017-04-05 23:45:47 +0200

    add test with negative numbers and refactor code

--- week04/day03/src/Fibonacci.java
+++ week04/day03/src/Fibonacci.java
@@ -4,13 +4,13 @@
 public class Fibonacci {
 
   public int fibonum(int n) {
-    if (n == 0) {
+    if (n < 0) {
+      return -1;
+    } else if (n == 0) {
       return 0;
-    }
-    else if (n == 1) {
+    } else if (n == 1) {
       return 1;
-    }
-    else {
+    } else {
       return fibonum(n - 1) + fibonum(n - 2);
     }
   }
--- week04/day03/src/FibonacciTest.java
+++ week04/day03/src/FibonacciTest.java
@@ -8,20 +8,26 @@ import org.junit.Test;
 public class FibonacciTest {
 
   @Test
-  public void Test1() {
+  public void TestWith1() {
     Fibonacci number = new Fibonacci();
     assertEquals(1, number.fibonum(1));
   }
 
   @Test
-  public void Test2() {
+  public void TestWith2() {
     Fibonacci number = new Fibonacci();
     assertEquals(1, number.fibonum(2));
   }
 
   @Test
-  public void Test3() {
+  public void TestWithN() {
     Fibonacci number = new Fibonacci();
     assertEquals(1597, number.fibonum(17));
+  }
+
+  @Test
+  public void TestWithNegative() {
+    Fibonacci number = new Fibonacci();
+    assertEquals(-1, number.fibonum(-7));
   }
 }
