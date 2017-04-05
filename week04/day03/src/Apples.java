1fe7c2e7f2e4d530abaa0bd03c0bcbba5483ac59
balintvecsey <balint.vecsey@gmail.com>
2017-04-05 11:09:45 +0200

    make day03 porject and add the first task

---
+++ week04/day03/src/Apples.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Bálint on 2017. 04. 05..
+ */
+public class Apples {
+  String apple;
+
+  public Apples(String apple) {
+    this.apple = apple;
+  }
+
+  public String getApple(){
+    return "apple";
+  }
+}
---
+++ week04/day03/src/ApplesTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Bálint on 2017. 04. 05..
+ */
+public class ApplesTest {
+
+  @Test
+  public void getApple() throws Exception {
+    Apples apples1 = new Apples("apple1");
+    assertEquals("apple", apples1.getApple());
+  }
+}
