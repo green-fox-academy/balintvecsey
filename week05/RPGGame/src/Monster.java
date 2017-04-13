ef439ee6faa5ef89e4f98087245760df5fe2e4ab
balintvecsey <balint.vecsey@gmail.com>
2017-04-13 11:30:46 +0200

    monsters don't spawn on walls

--- week05/RPGGame/src/Monster.java
+++ week05/RPGGame/src/Monster.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 /**
@@ -9,5 +10,15 @@ public class Monster extends GameObjects {
 
   public Monster(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
+  }
+
+  public void draw(Graphics graphics, Area area) {
+    if (!area.isWall(posX, posY)) {
+      graphics.drawImage(image, posX * TILE_WIDTH, posY * TILE_HEIGHT, null);
+    } else {
+      posX = (int) (Math.random() * 10);
+      posY = (int) (Math.random() * 10);
+      draw(graphics, area);
+    }
   }
 }
