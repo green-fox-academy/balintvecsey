8eb16fbd589c435ef091c9ffe7400b9fef5e6e84
balintvecsey <balint.vecsey@gmail.com>
2017-04-18 17:40:52 +0200

    add reversed length and and charAt

--- week06/day02/src/CharOperations.java
+++ week06/day02/src/CharOperations.java
@@ -2,15 +2,26 @@
  * Created by Bálint on 2017. 04. 18..
  */
 public class CharOperations implements CharSequence {
+  String reverse;
+
+  public static void main(String[] args) {
+    CharOperations word = new CharOperations("alma");
+
+    System.out.println(word.charAt(1));
+  }
+
+  public CharOperations(String line) {
+    this.reverse = line;
+  }
 
   @Override
   public int length() {
-    return 0;
+    return reverse.length();
   }
 
   @Override
   public char charAt(int index) {
-    return 0;
+    return reverse.charAt(reverse.length() - (index + 1));
   }
 
   @Override
