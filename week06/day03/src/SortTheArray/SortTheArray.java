5e801738ebb9fc40ca3f6ad485250e4c77c6d713
balintvecsey <balint.vecsey@gmail.com>
2017-04-19 16:08:25 +0200

    add solution for SortTheArray

---
+++ week06/day03/src/SortTheArray/SortTheArray.java
@@ -0,0 +1,22 @@
+package SortTheArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by Bálint on 2017. 04. 19..
+ */
+public class SortTheArray {
+
+  public static <T extends Number> void sortArray(T[] inputArray) {
+    try {
+      Arrays.sort(inputArray, Collections.reverseOrder());
+      for(T element : inputArray) {
+        System.out.printf("%s ", element);
+      }
+      System.out.println();
+    } catch (Exception e){
+      System.out.println();
+    }
+  }
+}
