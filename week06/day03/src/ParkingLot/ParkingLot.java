d8f60b55bcba9d8a27abf702712e107d04d76b8a
balintvecsey <balint.vecsey@gmail.com>
2017-04-21 02:19:55 +0200

    add mostCommonVechicle()

--- week06/day03/src/ParkingLot/ParkingLot.java
+++ week06/day03/src/ParkingLot/ParkingLot.java
@@ -8,9 +8,10 @@ import java.util.List;
  * Created by Bálint on 2017. 04. 19..
  */
 public class ParkingLot {
+
   List<Car> parkinglot = new ArrayList<>();
 
-  public ParkingLot(int size){
+  public ParkingLot(int size) {
     for (int i = 0; i < size; i++) {
       parkinglot.add(i, new Car(randomColor(), randomType()));
     }
@@ -25,7 +26,7 @@ public class ParkingLot {
 
     for (Car car : parkinglot) {
       String type = car.getType().toString();
-      if (typeCount.containsKey(type)){
+      if (typeCount.containsKey(type)) {
         typeCount.replace(type, typeCount.get(type) + 1);
       } else {
         typeCount.put(type, 1);
@@ -39,7 +40,7 @@ public class ParkingLot {
 
     for (Car car : parkinglot) {
       String color = car.getColor().toString();
-      if (colorCount.containsKey(color)){
+      if (colorCount.containsKey(color)) {
         colorCount.replace(color, colorCount.get(color) + 1);
       } else {
         colorCount.put(color, 1);
@@ -48,42 +49,59 @@ public class ParkingLot {
     System.out.println(colorCount);
   }
 
-  public ColorsOfCar randomColor(){
+  public void mostCommonVehicle(List<Car> parkinglot) {
+    HashMap<Car, Integer> common = new HashMap<>();
+
+    for (Car car : parkinglot) {
+      Integer val = common.get(car);
+      common.put(car, val == null ? 1 : val + 1);
+    }
+
+    HashMap.Entry<Car, Integer> max = null;
+
+    for (HashMap.Entry<Car, Integer> e : common.entrySet()) {
+      if (max == null || e.getValue() > max.getValue())
+        max = e;
+    }
+    System.out.println(max.getKey());
+  }
+  
+  public ColorsOfCar randomColor() {
     int d8 = (int) (Math.random() * 8) + 1;
-    if(d8 == 1){
+    if (d8 == 1) {
       return ColorsOfCar.RED;
-    } else if (d8 == 2){
+    } else if (d8 == 2) {
       return ColorsOfCar.BLUE;
-    } else if (d8 == 3){
+    } else if (d8 == 3) {
       return ColorsOfCar.GREEN;
-    } else if (d8 == 4){
+    } else if (d8 == 4) {
       return ColorsOfCar.YELOW;
-    } else if (d8 == 5){
+    } else if (d8 == 5) {
       return ColorsOfCar.GRAY;
-    } else if (d8 == 6){
+    } else if (d8 == 6) {
       return ColorsOfCar.BLACK;
-    } else if (d8 == 7){
+    } else if (d8 == 7) {
       return ColorsOfCar.WHITE;
     } else {
       return ColorsOfCar.ORANGE;
     }
   }
 
-  public TypeOfCar randomType(){
+  public TypeOfCar randomType() {
     int d8 = (int) (Math.random() * 8) + 1;
-    if(d8 == 1){
+    if (d8 == 1) {
       return TypeOfCar.TOYOTA;
-    } else if (d8 == 2){
+    } else if (d8 == 2) {
       return TypeOfCar.HONDA;
-    } else if (d8 == 3){
+    } else if (d8 == 3) {
       return TypeOfCar.OPEL;
-    } else if (d8 == 4){
+    } else if (d8 == 4) {
       return TypeOfCar.CITROEN;
-    } else if (d8 == 5){
+    } else if (d8 == 5) {
       return TypeOfCar.RENAULT;
-    } else if (d8 == 6){
+    } else if (d8 == 6) {
       return TypeOfCar.FIAT;
-    } else if (d8 == 7){
+    } else if (d8 == 7) {
       return TypeOfCar.VOLKSWAGEN;
     } else {
       return TypeOfCar.SKODA;
--- week06/day03/src/ParkingLot/ParkingLotApp.java
+++ week06/day03/src/ParkingLot/ParkingLotApp.java
@@ -1,5 +1,7 @@
 package ParkingLot;
 
+import java.util.Collections;
+
 /**
  * Created by Bálint on 2017. 04. 19..
  */
@@ -10,5 +12,6 @@ public class ParkingLotApp {
 
     p1.countAndPrintType(p1.getParkinglot());
     p1.countAndPrintColor(p1.getParkinglot());
+    p1.mostCommonVehicle(p1.getParkinglot());
   }
 }
