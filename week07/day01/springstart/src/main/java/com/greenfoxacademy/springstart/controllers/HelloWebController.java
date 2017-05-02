682e0d08a1d205222a3c39d2245c7d7ac7f8bcf7
balintvecsey <balint.vecsey@gmail.com>
2017-05-02 16:02:00 +0200

    add counter to the webapp

--- week07/day01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week07/day01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,18 +1,22 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by Bálint on 2017. 05. 02..
  */
 @Controller
 public class HelloWebController {
+  AtomicLong counter = new AtomicLong();
 
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(@RequestParam String name, Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.incrementAndGet());
     return "greeting";
   }
 }
