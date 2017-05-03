4c7732d4af6ba9758db5c579517042f16393e6f3
balintvecsey <balint.vecsey@gmail.com>
2017-05-03 11:23:21 +0200

    initial commit

---
+++ week07/day03/thymeleafstart/src/main/java/com/greenfoxacademy/thymeleafstart/ThymeleafstartApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.thymeleafstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafstartApplication.class, args);
+	}
+}
---
+++ week07/day03/thymeleafstart/src/test/java/com/greenfoxacademy/thymeleafstart/ThymeleafstartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.thymeleafstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafstartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
