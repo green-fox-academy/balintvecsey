8ef29413c01f5993fe1133a12b4da0142da91375
balintvecsey <balint.vecsey@gmail.com>
2017-05-04 16:58:25 +0200

    add index.html

--- week07/day05/programmerfoxclub/src/main/java/com/greenfoxacadamy/ProgrammerfoxclubApplication.java
+++ week07/day05/programmerfoxclub/src/main/java/com/greenfoxacadamy/ProgrammerfoxclubApplication.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy;
+package com.greenfoxacadamy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- week07/day05/programmerfoxclub/src/test/java/com/greenfoxacadamy/ProgrammerfoxclubApplicationTests.java
+++ week07/day05/programmerfoxclub/src/test/java/com/greenfoxacadamy/ProgrammerfoxclubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy;
+package com.greenfoxacadamy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
