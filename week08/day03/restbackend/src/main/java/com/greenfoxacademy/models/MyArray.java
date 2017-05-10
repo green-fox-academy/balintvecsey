5c2e69a199ead488d1454756b6e33c780f39ec48
balintvecsey <balint.vecsey@gmail.com>
2017-05-10 16:08:06 +0200

    started to work on ArrayHandler exercise

---
+++ week08/day03/restbackend/src/main/java/com/greenfoxacademy/models/MyArray.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Bálint on 2017. 05. 10..
+ */
+@Getter
+@Setter
+public class MyArray {
+  String what;
+  Integer[] numbers;
+
+}
