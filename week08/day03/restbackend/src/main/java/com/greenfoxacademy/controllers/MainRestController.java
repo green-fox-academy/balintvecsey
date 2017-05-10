125022d645ede06bd4f052227c94bdd7d2ec7a46
balintvecsey <balint.vecsey@gmail.com>
2017-05-10 13:17:33 +0200

    rename method name in /greeter endpoint

--- week08/day03/restbackend/src/main/java/com/greenfoxacademy/controllers/MainRestController.java
+++ week08/day03/restbackend/src/main/java/com/greenfoxacademy/controllers/MainRestController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.models.Doubling;
+import com.greenfoxacademy.models.Greeter;
 import com.greenfoxacademy.models.MyError;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,12 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainRestController {
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
-  public MyError errorParam() {
-    return new MyError("Please provide an input!");
+  public MyError errorParam(MissingServletRequestParameterException e) {
+    if(e.getParameterName().equals("input")) {
+      return new MyError("Please provide an " + e.getParameterName() + "!");
+    } else {
+      return new MyError("Please provide a " + e.getParameterName() + "!");
+    }
   }
 
   @RequestMapping(value = "/doubling", method = RequestMethod.GET)
   public Doubling doubling(@RequestParam(value = "input", required = true) int input) {
     return new Doubling(input);
+  }
+
+  @RequestMapping(value = "/greeter", method = RequestMethod.GET)
+  public Greeter greeter(@RequestParam(value = "name", required = true) String name, @RequestParam(value = "title", required = true) String title) {
+    return new Greeter("Oh, hi there " + name + ", my dear " + title + "!");
   }
 }
