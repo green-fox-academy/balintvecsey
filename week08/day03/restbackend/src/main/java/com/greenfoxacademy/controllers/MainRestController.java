466137c2a26292576165cbd6be7501c1450f4c95
balintvecsey <balint.vecsey@gmail.com>
2017-05-10 15:40:39 +0200

    add solution for Do until: add DoUntil and Until classes add /dountil/{what} endpoint add exception handler to handling request without number

--- week08/day03/restbackend/src/main/java/com/greenfoxacademy/controllers/MainRestController.java
+++ week08/day03/restbackend/src/main/java/com/greenfoxacademy/controllers/MainRestController.java
@@ -1,12 +1,16 @@
 package com.greenfoxacademy.controllers;
 
 import com.greenfoxacademy.models.AppendA;
+import com.greenfoxacademy.models.DoUntil;
 import com.greenfoxacademy.models.Doubling;
 import com.greenfoxacademy.models.Greeter;
 import com.greenfoxacademy.models.MyError;
+import com.greenfoxacademy.models.Until;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +31,11 @@ public class MainRestController {
     }
   }
 
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public MyError errorHttpRequest() {
+    return new MyError("Please provide a number!");
+  }
+
   @RequestMapping(value = "/doubling", method = RequestMethod.GET)
   public Doubling doubling(@RequestParam(value = "input", required = true) int input) {
     return new Doubling(input);
@@ -40,5 +49,23 @@ public class MainRestController {
   @RequestMapping(value = "/appenda/{appendable}", method = RequestMethod.GET)
   public AppendA appendA(@PathVariable String appendable) {
     return new AppendA(appendable);
+  }
+
+  @RequestMapping(value = "/dountil/{what}", method = RequestMethod.POST)
+  public DoUntil doUntil(@PathVariable String what, @RequestBody Until until) {
+    DoUntil doUntil = new DoUntil();
+    int result = 0;
+    if (what.equals("sum")) {
+      for (int i = 1; i <= until.getUntil(); i++) {
+        result += i;
+      }
+    } else if (what.equals("factor")) {
+      result = 1;
+      for (int i = 1; i <= until.getUntil(); i++) {
+        result *= i;
+      }
+    }
+    doUntil.setResult(result);
+    return doUntil;
   }
 }
---
+++ week08/day03/restbackend/src/main/java/com/greenfoxacademy/models/DoUntil.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Bálint on 2017. 05. 10..
+ */
+@Getter
+@Setter
+public class DoUntil {
+  private int result;
+
+}
