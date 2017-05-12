f73aedf546efd18420448007d6ac6f85e6aa1817
balintvecsey <balint.vecsey@gmail.com>
2017-05-12 10:26:10 +0200

    initial commit

---
+++ week08/day05/redditbackend/src/main/java/com/greenfox/balintvecsey/reddit/controllers/MainRESTController.java
@@ -0,0 +1,11 @@
+package com.greenfox.balintvecsey.reddit.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Bálint on 2017. 05. 12..
+ */
+@RestController
+public class MainRESTController {
+  
+}
