b7c6376713194d037a0fa458c4e7148de5492c61
balintvecsey <balint.vecsey@gmail.com>
2017-05-15 10:26:29 +0200

    initial commit

---
+++ week09/day01/testingrest/src/main/java/com/greenfoxacademy/TestingrestApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingrestApplication.class, args);
+	}
+}
