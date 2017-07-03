bd85561d8427c6783bef7ffef42270125021879b
balintvecsey <balint.vecsey@gmail.com>
2017-07-03 12:11:24 +0200

    print even numbers from given array

--- c#01/PracticeTasks/PracticeTasks/Program.cs
+++ c#01/PracticeTasks/PracticeTasks/Program.cs
@@ -10,6 +10,13 @@ namespace PracticeTasks
     {
         static void Main(string[] args)
         {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            int[] EvenNumbers = n.Where(i => i % 2 == 0).ToArray();
+            /*foreach (int i in EvenNumbers)
+            {
+                Console.WriteLine(i);
+            };*/
+            Console.ReadKey();
         }
     }
 }
