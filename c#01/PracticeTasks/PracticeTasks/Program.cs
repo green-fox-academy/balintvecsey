3aca5a61a8cdc595b0c5527399d6c17af83197e1
balintvecsey <balint.vecsey@gmail.com>
2017-07-03 12:15:29 +0200

    print in one line

--- c#01/PracticeTasks/PracticeTasks/Program.cs
+++ c#01/PracticeTasks/PracticeTasks/Program.cs
@@ -11,11 +11,12 @@ namespace PracticeTasks
         static void Main(string[] args)
         {
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
-            int[] EvenNumbers = n.Where(i => i % 2 == 0).ToArray();
-            /*foreach (int i in EvenNumbers)
+
+            IEnumerable<int> EvenNumbers = n.Where(i => i % 2 == 0);
+            foreach (int i in EvenNumbers)
             {
-                Console.WriteLine(i);
-            };*/
+                Console.Write(i + " ");
+            };
             Console.ReadKey();
         }
     }
