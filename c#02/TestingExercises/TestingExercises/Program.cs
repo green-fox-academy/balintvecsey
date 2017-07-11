387996024a57d2b0dbbeccabfebd546a53d18752
balintvecsey <balint.vecsey@gmail.com>
2017-07-11 12:33:36 +0200

    add testcase for true anagram

--- c#02/TestingExercises/TestingExercises/Program.cs
+++ c#02/TestingExercises/TestingExercises/Program.cs
@@ -13,11 +13,14 @@ namespace TestingExercises
             string word1 = "alma";
             string word2 = "lama";
 
-            Console.WriteLine(Anagram(word1, word2));
+            //Console.WriteLine(Anagram.IsAnagram(word1, word2));
             Console.ReadLine();
         }
+    }
 
-        public static bool Anagram(string word1, string word2)
+    public class Anagram
+    {
+        public bool IsAnagram(string word1, string word2)
         {
             char[] chars1 = word1.ToCharArray();
             char[] chars2 = word2.ToCharArray();
--- c#02/TestingExercises/TestingExercisesTests/AnagramTests.cs
+++ c#02/TestingExercises/TestingExercisesTests/AnagramTests.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingExercises;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestingExercisesTests
+namespace TestingExercises.Tests
 {
-    class AnagramTests
+    [TestClass()]
+    public class AnagramTests
     {
+        Anagram anagram;
+        string word1;
+        string word2;
+
+        public AnagramTests()
+        {
+            anagram = new Anagram();
+        }
+
+        [TestMethod()]
+        public void TestWithAnagram()
+        {
+            word1 = "alma";
+            word2 = "lama";
+
+            Assert.IsTrue(anagram.IsAnagram(word1, word2));
+        }
     }
 }
