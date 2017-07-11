2c8056e9f6126033f585c1d976d74a06fd9c8e8c
balintvecsey <balint.vecsey@gmail.com>
2017-07-11 11:40:35 +0200

    add Anagram exercise

--- c#02/TestingExercises/TestingExercises/Program.cs
+++ c#02/TestingExercises/TestingExercises/Program.cs
@@ -10,6 +10,22 @@ namespace TestingExercises
     {
         static void Main(string[] args)
         {
+            string word1 = "alma";
+            string word2 = "lama";
+
+            Console.WriteLine(Anagram(word1, word2));
+            Console.ReadLine();
+        }
+
+        public static bool Anagram(string word1, string word2)
+        {
+            char[] chars1 = word1.ToCharArray();
+            char[] chars2 = word2.ToCharArray();
+
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+
+            return chars1.SequenceEqual(chars2);
         }
     }
 }
