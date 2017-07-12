09f95a480cead4487e8a8e8155d3943d1de89c6d
balintvecsey <balint.vecsey@gmail.com>
2017-07-12 15:29:59 +0200

    add solution for Problem 1

--- algorithm/src/Algorithm.java
+++ algorithm/src/Algorithm.java
@@ -1,4 +1,17 @@
 public class Algorithm {
 
+  public static void main(String[] args) {
+    System.out.println(sumOfTheMultiplesOf3and5(1000));
+  }
 
+  public static int sumOfTheMultiplesOf3and5(int range) {
+    int sum = 0;
+
+    for (int i = 0; i < range; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
 }
