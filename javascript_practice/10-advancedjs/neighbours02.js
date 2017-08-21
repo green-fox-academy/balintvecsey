b49b9c39b1fd6d1ef1f6d364c24859d64df11476
balintvecsey <balint.vecsey@gmail.com>
2017-08-21 15:57:54 +0200

    add solution for neighbours02

--- javascript_practice/10-advancedjs/neighbours02.js
+++ javascript_practice/10-advancedjs/neighbours02.js
@@ -3,16 +3,15 @@
 // This code isn't working properly. The function should print: 'The result is 6'
 // Could you find the appropriate types of the variables?
 // Where can you invocate the function?
+const person = 2;
 
 amountOfBeer(person);
-
-const person = 2;
 
 function amountOfBeer(person) {
   var i = person;
 
-  for (var i = 0; i <= 4; i++) {
-    let j = i;
+  for (let i = 0; i <= 4; i++) {
+    var j = i;
   }
   console.log('The result is:', i + j);
 }
