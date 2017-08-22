4bd39c3b81403a48b6ff411dbc2e897ef274cbcc
balintvecsey <balint.vecsey@gmail.com>
2017-08-22 12:44:13 +0200

    add solution for sharpie task

---
+++ javascript_practice/10-advancedjs/sharpie.js
@@ -0,0 +1,13 @@
+function Sharpie(color, width) {
+  this.color =  color,
+  this.width = width,
+  this.inkAmount = 100,
+  this.use =  function() {
+    this.inkAmount -= width;
+  }
+}
+
+let redSharpie = new Sharpie('red', 10);
+console.log(redSharpie.inkAmount);
+redSharpie.use();
+console.log(redSharpie.inkAmount);
