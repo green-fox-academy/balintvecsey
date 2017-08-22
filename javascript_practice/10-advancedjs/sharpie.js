791bd3e7f1f6caf7bd5e4d38f716b513263af087
balintvecsey <balint.vecsey@gmail.com>
2017-08-22 12:49:53 +0200

    add semicolons

--- javascript_practice/10-advancedjs/sharpie.js
+++ javascript_practice/10-advancedjs/sharpie.js
@@ -1,10 +1,10 @@
 function Sharpie(color, width) {
-  this.color =  color,
-  this.width = width,
-  this.inkAmount = 100,
+  this.color =  color;
+  this.width = width;
+  this.inkAmount = 100;
   this.use =  function() {
     this.inkAmount -= width;
-  }
+  };
 }
 
 let redSharpie = new Sharpie('red', 10);
