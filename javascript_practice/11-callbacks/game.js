a86dc48ae3525afedc398adb6b6e45a4962abcda
balintvecsey <balint.vecsey@gmail.com>
2017-08-25 17:01:56 +0200

    add addLollypop function to its button

--- javascript_practice/11-callbacks/game.js
+++ javascript_practice/11-callbacks/game.js
@@ -9,7 +9,16 @@ let countSpeed = document.querySelector('.speed');
 let currentCandies = 0;
 
 function addCandy() {
-  countCandies.innerHTML = ++ currentCandies;
+  countCandies.innerHTML = ++currentCandies;
+}
+
+function addLollypop() {
+  if(currentCandies >= 100) {
+    countLollypops.innerHTML += '🍭';
+    countCandies.innerHTML = 0;
+    currentCandies = 0;
+  }
 }
 
 buttonCreateCandies.addEventListener('click', addCandy);
+buttonBuyLollypops.addEventListener('click', addLollypop);
