9888b3cbd871c538127edce29cbc7b310f97b808
balintvecsey <balint.vecsey@gmail.com>
2017-08-25 16:24:21 +0200

    define dom elements

---
+++ javascript_practice/11-callbacks/game.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let buttonCreateCandies = document.querySelector('create-candies');
+let buttonBuyLollypops = document.querySelector('buy-lollypops');
+let buttonCandyMachine = document.querySelector('candy-machine');
+let countCandies = document.querySelector('candies');
+let countLollypops = document.querySelector('lollypops');
+let countSpeed = document.querySelector('speed');
