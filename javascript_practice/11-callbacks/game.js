82a90c5c07bd2c8ad035d1907e53067bf578a4b9
balintvecsey <balint.vecsey@gmail.com>
2017-08-25 16:45:46 +0200

    add addCandy function to its button

--- javascript_practice/11-callbacks/game.js
+++ javascript_practice/11-callbacks/game.js
@@ -1,8 +1,15 @@
 'use strict';
 
-let buttonCreateCandies = document.querySelector('create-candies');
-let buttonBuyLollypops = document.querySelector('buy-lollypops');
-let buttonCandyMachine = document.querySelector('candy-machine');
-let countCandies = document.querySelector('candies');
-let countLollypops = document.querySelector('lollypops');
-let countSpeed = document.querySelector('speed');
+let buttonCreateCandies = document.querySelector('.create-candies');
+let buttonBuyLollypops = document.querySelector('.buy-lollypops');
+let buttonCandyMachine = document.querySelector('.candy-machine');
+let countCandies = document.querySelector('.candies');
+let countLollypops = document.querySelector('.lollypops');
+let countSpeed = document.querySelector('.speed');
+let currentCandies = 0;
+
+function addCandy() {
+  countCandies.innerHTML = ++ currentCandies;
+}
+
+buttonCreateCandies.addEventListener('click', addCandy);
