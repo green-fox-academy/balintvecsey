9b16da7f4438a214914c017e58be807f05ae049e
balintvecsey <balint.vecsey@gmail.com>
2017-08-25 13:55:29 +0200

    add solution for caller

--- javascript_practice/11-callbacks/caller.js
+++ javascript_practice/11-callbacks/caller.js
@@ -3,6 +3,15 @@
 // Implement the selectLastEvenNumber function that takes an array and callback,
 // it should call the callback immediately with the last even number on the array
 
+function selectLastEvenNumber(array, callback) {
+  let lastEvenNum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] % 2 === 0) {
+      lastEvenNum = array[i];
+    }
+  }
+  callback(lastEvenNum);
+}
 
 function printNumber(num) {
   console.log(num);
