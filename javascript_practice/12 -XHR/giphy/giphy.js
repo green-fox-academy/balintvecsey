86b7c140773ed00f773a6d9c7a8e9e14400701fb
balintvecsey <balint.vecsey@gmail.com>
2017-08-28 17:43:04 +0200

    add random button to get another gif

--- javascript_practice/12 -XHR/giphy/giphy.js
+++ javascript_practice/12 -XHR/giphy/giphy.js
@@ -2,15 +2,23 @@
 
 let httpRequest = new XMLHttpRequest();
 let img = document.querySelector('img');
+let randomButton = document.querySelector('button');
 
-httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=funny+cute&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
-httpRequest.send();
+let getRandomImage = function(){
+  httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=funny+cute&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
+  httpRequest.send();
+  httpRequest.onreadystatechange = function() {
+    if(httpRequest.readyState === 4 && httpRequest.status === 200) {
+      let response = JSON.parse(httpRequest.response);
+      let data = response.data;
+      console.log(response);
+      img.src = data.image_original_url;
+    }
+  };
+}
 
-httpRequest.onreadystatechange = function() {
-  if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-    let response = JSON.parse(httpRequest.response);
-    let data = response.data;
-    console.log(response);
-    img.src = data.image_original_url;
-  }
-};
+getRandomImage();
+randomButton.addEventListener('click', function() {
+  getRandomImage();
+  console.log('click');
+});
