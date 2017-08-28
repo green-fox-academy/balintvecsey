e4eecf4bf278dae88217c42f029c2505a95cc3f0
balintvecsey <balint.vecsey@gmail.com>
2017-08-28 17:27:49 +0200

    add random gif from giphy

--- javascript_practice/12 -XHR/giphy/giphy.js
+++ javascript_practice/12 -XHR/giphy/giphy.js
@@ -1,6 +1,16 @@
 'use strict';
 
 let httpRequest = new XMLHttpRequest();
+let img = document.querySelector('img');
+
 httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=funny+cute&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
 httpRequest.send();
-httpRequest.onreadystatechange = console.log;
+
+httpRequest.onreadystatechange = function() {
+  if(httpRequest.readyState === 4 && httpRequest.status === 200) {
+    let response = JSON.parse(httpRequest.response);
+    let data = response.data;
+    console.log(response);
+    img.src = data.image_original_url;
+  }
+};
