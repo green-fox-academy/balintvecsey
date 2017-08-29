a5d879ef420514063834e17b15d1d568fbe1d856
balintvecsey <balint.vecsey@gmail.com>
2017-08-29 13:48:11 +0200

    add rick and morty tag and a picture for loading time

--- javascript_practice/12 -XHR/giphy/giphy.js
+++ javascript_practice/12 -XHR/giphy/giphy.js
@@ -5,16 +5,17 @@ let img = document.querySelector('img');
 let randomButton = document.querySelector('button');
 
 let getRandomImage = function(){
-  httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=funny+cute&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
+  httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=rick+morty+rick-and-morty&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
   httpRequest.send();
   httpRequest.onreadystatechange = function() {
     if(httpRequest.readyState === 4 && httpRequest.status === 200) {
       let response = JSON.parse(httpRequest.response);
       let data = response.data;
       console.log(response);
-      img.src = data.image_original_url;
+      img.src = data.image_url;
     }
   };
+  img.src = 'adasszunet.jpg';
 }
 
 getRandomImage();
