63774a2390de7379cc18f1b2eb17d32e863baed5
balintvecsey <balint.vecsey@gmail.com>
2017-08-29 14:11:35 +0200

    onreadystatechange can be global

--- javascript_practice/12 -XHR/giphy/giphy.js
+++ javascript_practice/12 -XHR/giphy/giphy.js
@@ -7,16 +7,17 @@ let randomButton = document.querySelector('button');
 let getRandomImage = function(){
   httpRequest.open('GET', 'http://api.giphy.com/v1/gifs/random?tag=rick+morty+rick-and-morty&api_key=eb56f93106b0410b81efd2215907147a&limit=1', true);
   httpRequest.send();
-  httpRequest.onreadystatechange = function() {
-    if(httpRequest.readyState === 4 && httpRequest.status === 200) {
-      let response = JSON.parse(httpRequest.response);
-      let data = response.data;
-      console.log(response);
-      img.src = data.image_url;
-    }
-  };
   img.src = 'adasszunet.jpg';
-}
+};
+
+httpRequest.onreadystatechange = function() {
+  if(httpRequest.readyState === 4 && httpRequest.status === 200) {
+    let response = JSON.parse(httpRequest.response);
+    let data = response.data;
+    console.log(response);
+    img.src = data.image_url;
+  }
+};
 
 getRandomImage();
 randomButton.addEventListener('click', function() {
